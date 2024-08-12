45458b83f7d76548ba24448817011f3954bd0a18
kailerlaino <144497906+kailerlaino@users.noreply.github.com>
2024-08-11 22:33:28 -0500

    deleted analytics in firebase

--- firebase.js
+++ firebase.js
@@ -1,6 +1,5 @@
 // firebase.js
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
 
 // Firebase configuration from environment variables
@@ -16,7 +15,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const firestore = getFirestore(app);
 
 export { firestore };
